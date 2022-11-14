324c45db5fdea86038bee9c5bc586a1a7fa1166b
anatoly <tolyan.yalta@yandex.ru>
2022-11-14 18:41:44 +0300

    Add file export_data_csv.py version 2.0

--- export_data_csv.py
+++ export_data_csv.py
@@ -1,8 +1,9 @@
 import csv
 
 def write_new_data_csv(new_data_csv):
+    # Запись данных в формате csv
     with open('data.csv', 'a', encoding='utf-8') as f:
-            w = csv.writer(f, lineterminator="\n")
-            w.writerows(new_data_csv)
+        w = csv.writer(f, lineterminator="\n")
+        w.writerows(new_data_csv)
 
 
