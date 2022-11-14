d0eeca99128c93aff73de1708dcf5d4037c281ea
anatoly <tolyan.yalta@yandex.ru>
2022-11-14 20:26:58 +0300

    Add file controller.py version 2.0

--- controller.py
+++ controller.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 import view
 import export_data_csv
+import export_data_xml
 import import_data_csv
 import import_data_xml
-
-
 
 def press_btn_data_csv():
     # Считывает данные из файла csv и отправляет на вывод в окно
@@ -17,30 +16,42 @@ def press_btn_data_xml():
     view.main_window(data)
 
 
-# def start():
-task = tk.Tk()
-task.title('Домашнее задание')
+def start():
+    task = tk.Tk()
+    task.title('Домашнее задание')
 
-h = 400
-w = 400
-task.geometry(f"{w}x{h}+100+100")
-task.resizable(False, False)
+    h = 160
+    w = 400
+    task.geometry(f"{w}x{h}+100+100")
+    task.resizable(False, False)
 
-def press_btn_add_data():
-    view.add_data_window()
-    new_data_csv = view.new_data_csv_to_controller()
-    # print([new_data_csv])
-    export_data_csv.write_new_data_csv([new_data_csv])
+    def press_btn_add_data():
+        # Запускает виджет ввода новых данных
+        # Запрашивает новые данные из модуля view
+        # Запускает виджет выбора варианта сохранения данных
+        # Запрашивает выбранный вариант сохранения данных
+        # Передает данные в соответствующий модуль
+        view.add_data_window()
+        new_data = view.new_data_to_controller()
+        view.choosing_save_option()
+        var_save = view.variant_save_to_controller()
+        if var_save == 'csv':
+            export_data_csv.write_new_data_csv([new_data])
+        elif var_save == 'xml':
+            new_data_xml = [*new_data]
+            export_data_xml.write_new_data_xml(new_data_xml)
 
+    def programm_exit():
+        task.quit()
 
-def programm_exit():
-    task.quit()
-
-btn_data_csv = tk.Button(task, text='Показать телефонный справочник (csv)', command=press_btn_data_csv, font=('times new roman',14, 'bold')).pack(fill='x')
-btn_data_xml = tk.Button(task, text='Показать телефонный справочник (xml)', command=press_btn_data_xml, font=('times new roman',14, 'bold')).pack(fill='x')
-
-btn_add_data = tk.Button(task, text='Добавить данные в справочник', command=press_btn_add_data, font=('times new roman',14, 'bold')).pack(fill='x')
-btn_quit = tk.Button(task, text='Выйти из программы', command=programm_exit, font=('times new roman',14, 'bold')).pack(fill='x')
-task.mainloop()
+    btn_data_csv = tk.Button(task, text='Показать телефонный справочник (csv)', command=press_btn_data_csv, font=('times new roman',14, 'bold'))
+    btn_data_csv.pack(fill='x')
+    btn_data_xml = tk.Button(task, text='Показать телефонный справочник (xml)', command=press_btn_data_xml, font=('times new roman',14, 'bold'))
+    btn_data_xml.pack(fill='x')
+    btn_add_data = tk.Button(task, text='Добавить данные в справочник', command=press_btn_add_data, font=('times new roman',14, 'bold'))
+    btn_add_data.pack(fill='x')
+    btn_quit = tk.Button(task, text='Выйти из программы', command=programm_exit, font=('times new roman',14, 'bold'))
+    btn_quit.pack(fill='x')
+    task.mainloop()
 
 
