6a5f8630bc27152bde9ec0744f7e77306446651e
anatoly <tolyan.yalta@yandex.ru>
2022-11-14 18:42:34 +0300

    Add file import_data_csv.py version 2.0

--- import_data_csv.py
+++ import_data_csv.py
@@ -1,6 +1,7 @@
 import csv
 
 def read_data_csv():
+    # Чтение данных из файла в формате csv
     with open('data.csv', 'r', newline='', encoding='utf-8') as f:
         data = [tuple(row) for row in csv.reader(f)]
     return data
